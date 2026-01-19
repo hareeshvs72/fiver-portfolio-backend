da1c5229fbcf6b3250ecbe6ff2ff87d3f8a0265d
hareeshvs72 <hareeshvs72@gmail.com>
2026-01-19 13:24:57 +0530

    update / url

--- server.js
+++ server.js
@@ -12,7 +12,11 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/projects", projectRoutes);
+    app.use("/api/projects", projectRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+app.get('/',(req,res)=>{
+ res.send("<h1>Server Is running</h1>")
+})
